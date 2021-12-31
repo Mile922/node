dcb16f4cd98a84ca2a9521c75ba4556f54e0b33f
wt627 <2214166877@qq.com>
2021-12-30 22:39:58 -0800

    loginregister

--- node.js
+++ node.js
@@ -2,44 +2,73 @@ const express = require("express")
 const app = express()
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://172.21.2.236:27017/190110910627');
+mongoose.connection.once("open",err=>{
+    if(err)
+        throw err;
+    console.log("database connection success!!");
+})
 const schema = {
-    username:String,
-    password:String
+    username:{
+        type:String,
+        unique:true,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    }
 
 }
 const mydata = mongoose.model('testuser02', schema);
+const mydata1 = mongoose.model('manager', schema);
 app.use('/',express.static('public'))
 
-app.post("/register",(req,res)=>{
+app.get("/register",(req,res)=>{
     console.log(req.query)
     // 获取用户提交的信息
     var postData = {
         username: req.query.username,
-        password: req.query.password
+        password: req.query.password,
 
     };
     mydata.findOne({username: postData.username}, function (err, data){
         if (data) {
             res.send('用户名已被注册');
         } else{
-            const user = new mydata({ username: req.query.username,password:req.query.password });
-            user.save()
+            const u = new mydata({ username: req.query.username,password:req.query.password });
+            u.save();
             console.log('注册成功');
-            res.sendfile('./public/login.html'); 
+            res.sendfile('./public/userlogin.html'); 
         }
 
     })
 });
+//管理员登录 只能用 wt 1246
+app.get("/managelogin",(req,res)=>{
+	var postData = {
+        username: req.query.username,
+        password: req.query.password,
+    };
+    mydata1.findOne({username: postData.username,password: postData.password}, function (err, data) {
+        console.log('data',data)
+        if(data){
+            res.send('管理员页面');
+        }else{
+            res.send('账号或密码错误')
+        }
+    } )
+});
 
-app.post("/login",(req,res)=>{
+//用户登录
+app.get("/login",(req,res)=>{
 	var postData = {
         username: req.query.username,
         password: req.query.password,
     };
     mydata.findOne({username: postData.username,password: postData.password}, function (err, data) {
         console.log('data',data)
-        if(data._doc.username){
-            res.send('登录成功');
+        if(data){
+            res.send('用户页面');
         }else{
             res.send('账号或密码错误')
         }
